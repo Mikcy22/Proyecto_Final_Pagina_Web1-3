b0f2341479149f6a095d727269aa5db26b55bef3
Micky22 <Woodhercules22@gmail.com>
2023-01-08 20:35:07 +0100

    Carrusel
    Carrusel de fotos , con retoques en los comentarios

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 //Mi propio JS
 function toggleMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
   }
-  
+}
+
+  //CODIGO EXTERNO
+
+ // Obtener todas las diapositivas
+ var slides = document.querySelectorAll(".slide");
+ // Inicialmente, mostrar la primera diapositiva
+ var currentSlide = 0;
+ slides[currentSlide].style.display = "block";
+
+ // Función para avanzar a la siguiente diapositiva
+ function nextSlide() {
+   // Ocultar la diapositiva actual
+   slides[currentSlide].style.display = "none";
+   // Incrementar el índice de la diapositiva actual
+   currentSlide++;
+   // Si llegamos al final, volver al primer slide
+   if (currentSlide >= slides.length) {
+     currentSlide = 0;
+   }
+   // Mostrar la diapositiva actual
+   slides[currentSlide].style.display = "block";
+ }
+
+ // Ejecutar la función "nextSlide" cada 4 segundos (4000 milisegundos)
+ setInterval(nextSlide, 4000);
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Todos los campos estan en el formulario de registro menos el comentario
+el cual esta en index.
